a527445e01f419ea1022468314bb1dd8a70149b5
Carlos Rodrigo <carlos.martinez9@catolica.edu.sv>
2025-02-06 19:46:57 -0600

    Implementacion del gitignore

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efac468db24ab75aab7f23f108e991022637617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d05a0590033d8e80cf9ba536cc6705dfa1428b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

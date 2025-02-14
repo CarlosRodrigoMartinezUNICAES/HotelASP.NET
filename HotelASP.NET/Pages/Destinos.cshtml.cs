3e3802b0148db69c9f8ca9cbb490dec38820ce61
Jeferson Chavez <jeferson.chavez@catolica.edu.sv>
2025-02-13 18:01:01 -0600

    implementacion de pagina de destinos

---
+++ HotelASP.NET/Pages/Destinos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyReverHotels.Pages
+{
+    public class DestinosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

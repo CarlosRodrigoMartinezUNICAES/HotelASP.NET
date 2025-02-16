3a9a538820d2bd0ed6231cf063c248d949d16759
Carlos <Carlos@DESKTOP-DEBD1DU>
2025-02-15 23:00:32 -0600

    Implementacion de nueva pagina "Contactos" y "Ofertas_Especiales"

---
+++ HotelASP.NET/Pages/Ofertas_Especiales.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelASP.NET.Pages
+{
+    public class OfertasEspecialesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

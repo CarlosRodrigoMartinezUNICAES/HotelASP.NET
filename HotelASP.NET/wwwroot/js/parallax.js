0e7e18263687c066bf62f291d44dd310cc7136ac
Carlos <Carlos@DESKTOP-DEBD1DU>
2025-02-27 18:19:22 -0600

    Arreglado visualizacion de texto en Index, y añadido efecto parallax para fondo y cards en Destinos

---
+++ HotelASP.NET/wwwroot/js/parallax.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("mousemove", (event) => {
+    const { clientX, clientY } = event;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const moveX = Math.max(-20, Math.min((clientX - centerX) * 0.02, 20));
+    const moveY = Math.max(-0, Math.min((clientY - centerY) * 0.02, 0));
+
+    document.querySelectorAll(".destination-card img").forEach(img => {
+        img.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.1)`;
+    });
+});
---
+++ HotelASP.NET/wwwroot/js/prlxbackground.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("mousemove", (event) => {
+    const { clientX, clientY } = event;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const moveX = Math.max(-30, Math.min((clientX - centerX) * 0.02, 30));
+    const moveY = Math.max(-30, Math.min((clientY - centerY) * 0.02, 30));
+
+    const parallaxBackground = document.querySelector(".parallax-background");
+
+    // Apply the transformation to the parallax background
+    parallaxBackground.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.2)`;
+});
